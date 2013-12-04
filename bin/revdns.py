c559fc14b6930ab1848f440f573146d6f4146809
Marcio Paiva <marcioapf@gmail.com>
2013-12-04 07:32:04 -0200

    Avoiding crash with hostname lookup error
    In the eventuality of a problem in the parsing of dig -x output (used to find hostname), the program simply uses the IP address instead of crashing.

--- bin/revdns.py
+++ bin/revdns.py
@@ -9,12 +9,21 @@ def reverseIp(ip):
 
 def hostnameResolved(digoutput):
 	match = re.search('ANSWER:\s*(\d)', digoutput);
+	if (match is None) or (match.lastindex is None):
+		print("Could not find 'ANSWER:' in dig -x output")
+		return False
+	
 	return int(match.group(1)) > 0
 
 
 def hostnameFromDig(digoutput, ip):
 	print("searching for hostname of " + ip),
 	match = re.search('^' + reverseIp(ip) + '.*PTR\s+(\S+)', digoutput, flags=re.MULTILINE)
+
+	if (match is None) or (match.lastindex is None):
+		print("Could not parse dig -x output")
+		return ip
+
 	print("\tfound: " + match.group(1))
 	return match.group(1)
 
